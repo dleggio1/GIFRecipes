d752245af2281e92bda99902925ab0f46af5fbf7
Dylan Leggio <dleggio1@binghamton.edu>
2016-11-02 17:08:52 +0000

    Moved from sqlite3 to postgresql for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161029183243) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "gifs", force: :cascade do |t|
     t.string   "title"
     t.string   "link"
